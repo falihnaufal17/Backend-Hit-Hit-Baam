cd9c82408829992389f191a4c734bee49d692f20
falihnaufal17 <dudu.kotkot@gmail.com>
2019-08-16 02:00:54 +0700

    add image for user

--- src/models/score.js
+++ src/models/score.js
@@ -3,7 +3,7 @@ const conn = require('../configs/db')
 module.exports = {
     getAllScore: () => {
         return new Promise((resolve, reject) => {
-            conn.query('SELECT skor.idskor, user.iduser, user.fullname, skor.skor, skor.created_at, skor.updated_at FROM skor INNER JOIN user ON skor.iduser = user.iduser ORDER BY skor.skor DESC LIMIT 10 OFFSET 0', (err, result) => {
+            conn.query('SELECT skor.idskor, user.iduser, user.fullname, user.image, skor.skor, skor.created_at, skor.updated_at FROM skor INNER JOIN user ON skor.iduser = user.iduser ORDER BY skor.skor DESC LIMIT 10 OFFSET 0', (err, result) => {
                 if (!err) {
                     resolve(result)
                 } else {
